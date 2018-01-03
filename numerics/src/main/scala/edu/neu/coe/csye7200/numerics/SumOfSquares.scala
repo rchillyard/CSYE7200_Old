f1892541fd0b075256d7bd5b7c1807df5bb57c6c
yingyuan <yingy4@hotmail.com>
2018-01-03 17:34:37 +0800

    Add numerics module

---
+++ numerics/src/main/scala/edu/neu/coe/csye7200/numerics/SumOfSquares.scala
@@ -0,0 +1,10 @@
+package edu.neu.coe.csye7200.numerics
+
+/**
+  * Created by scalaprof on 10/28/16.
+  */
+object SumOfSquares extends App {
+  def sumOfSquares(xs: Seq[Double]) = xs map (x => x * x) sum
+
+  println(sumOfSquares(Seq(1,2,3,4,5)))
+}
---
+++ numerics/src/main/scala/edu/neu/coe/csye7200/parse/ExpressionParser.scala
@@ -0,0 +1,45 @@
+package edu.neu.coe.csye7200.parse
+
+import scala.util.parsing.combinator._
+import scala.util.Try
+
+/**
+ * @author scalaprof
+ */
+abstract class ExpressionParser[T] extends JavaTokenParsers with (String => Try[T]) { self =>
+
+  def apply(s: String): Try[T]
+  def div: (T,T)=>T
+  def negate: (T)=>T
+  def plus: (T,T)=>T
+  def times: (T,T)=>T
+  def one: T
+  def zero: T
+  
+  trait Expression {
+    def value: Try[T]
+  }
+
+  def lift(t: Try[T])(f: (T) => T): Try[T] = t map f
+  def map2(t1: Try[T], t2: Try[T])(f: (T,T) => T): Try[T] = for { tt1 <- t1 ; tt2 <- t2 } yield f(tt1,tt2)
+  
+  abstract class Factor extends Expression
+  case class Expr(t: Term, ts: List[String~Term]) extends Expression {
+    def termVal(t: String~Term): Try[T] = t match {case "+"~x => x.value; case "-"~x => lift(x.value)(negate) }
+    def value = ts.foldLeft(t.value)((a,x) => map2(a,termVal(x))(plus))
+  }
+  case class Term(f: Factor, fs: List[String~Factor]) extends Expression {
+    def factorVal(t: String~Factor): Try[T] = t match {case "*"~x => x.value; case "/"~x => map2(Try(one),x.value)(div) }
+    def value = fs.foldLeft(f.value)((a,x)=>map2(a,factorVal(x))(times))
+  }
+  case class FloatingPoint(x: String) extends Factor {
+    def value = self.apply(x)
+  }
+  case class Parentheses(e: Expr) extends Factor {
+    def value = e.value
+  }
+  
+  def expr: Parser[Expr] = term~rep("+"~term | "-"~term | failure("expr")) ^^ { case t~r => r match {case x: List[String~Term] => Expr(t,x)}}
+  def term: Parser[Term] = factor~rep("*"~factor | "/"~factor | failure("term")) ^^ { case f~r => r match {case x: List[String~Factor] => Term(f,x)}} 
+  def factor: Parser[Factor] = (floatingPointNumber | "("~expr~")" | failure("factor")) ^^ { case "("~e~")" => e match {case x: Expr => Parentheses(x)}; case s: String => FloatingPoint(s) }
+}
