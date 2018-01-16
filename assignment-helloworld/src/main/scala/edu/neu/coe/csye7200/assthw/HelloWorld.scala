e49fec951a0fe6c705ba180623c463559c174df0
yingyuan <yingy4@hotmail.com>
2018-01-16 15:05:46 -0500

    Assignment HelloWorld: Add instruction and csv file needed.

--- assignment-helloworld/src/main/scala/edu/neu/coe/csye7200/assthw/HelloWorld.scala
+++ assignment-helloworld/src/main/scala/edu/neu/coe/csye7200/assthw/HelloWorld.scala
@@ -2,6 +2,12 @@ package edu.neu.coe.csye7200.assthw
 
 /**
   * Created by scalaprof on 8/25/16.
+  *
+  * This is the first part of assignment,
+  * You should able to run this HelloWorld and
+  * HelloWorldSpec under test folder,
+  * Your submission should be the screenshot of
+  * running HelloWorldSpec and pass the test.
   */
 object HelloWorld extends App {
   def greeting = "Hello World!"
