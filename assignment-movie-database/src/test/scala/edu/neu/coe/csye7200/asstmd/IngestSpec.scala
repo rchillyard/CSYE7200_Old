ed0e0d3c40f9873da873147eed4d533c41622eac
yingyuan <yingy4@hotmail.com>
2018-01-24 14:30:41 -0500

    Assignment-movie-database: update instruction for todo
    (cherry picked from commit c012a81d28c859fd7c3cfebee5ded1fd569deeed)

--- assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
+++ assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
@@ -20,7 +20,7 @@ class IngestSpec extends FlatSpec with Matchers {
     val source = Source.fromChars(Array('x', '\n', '4', '2'))
     val ingester = new Ingest[Int]()
     val xys = ingester(source).toSeq
-    // TODO check that xys has one element, consisting of Success(42) -- 10 points
+    // TODO check that xys has exactly one element, consisting of Success(42) -- 10 points
     ???
   }
 
