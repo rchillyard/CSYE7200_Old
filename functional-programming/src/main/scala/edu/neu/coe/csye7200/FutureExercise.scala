c7e66ee7d2f8e72b4c50045fa04df09bba28b0b9
Scala Prof <scalaprof@gmail.com>
2018-02-12 10:17:49 -0500

    Improved FutureExercise

--- functional-programming/src/main/scala/edu/neu/coe/csye7200/FutureExercise.scala
+++ functional-programming/src/main/scala/edu/neu/coe/csye7200/FutureExercise.scala
@@ -1,21 +1,18 @@
 package edu.neu.coe.csye7200
 
-import scala.collection.immutable.IndexedSeq
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.util._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by scalaprof on 2/17/17.
   */
 object FutureExercise extends App {
 
-  val chunk = 1000000 // Try it first with chunk = 1000 and build up to 1000000
-
+  val chunk = 10000 // Try it first with chunk = 10000 and build up to 1000000
   def integers(i: Int, n: Int): Stream[Int] = Stream.from(i) take n
 
-  def sum[N : Numeric](is: Stream[N]): BigInt = is.foldLeft(BigInt(0))(_+implicitly[Numeric[N]].toLong(_))
+  def sum[N: Numeric](is: Stream[N]): BigInt = is.foldLeft[BigInt](0)(_ + implicitly[Numeric[N]].toLong(_))
 
   def asyncSum(is: Stream[Int]): Future[BigInt] = Future {
     val x = sum(is)
@@ -23,19 +20,13 @@ object FutureExercise extends App {
     x
   }
 
-  val xfs = for (i <- 0 to 10) yield asyncSum(integers(i * chunk, chunk))
+  val xfs = for (i <- 0 to 9) yield asyncSum(integers(i * chunk + 1, chunk))
+  val xsf = Future.sequence(xfs)
+  val xf = for (ls <- xsf) yield sum(ls.toStream)
+  xf foreach { x => println(s"Sum: $x") }
+  private val expected = xf filter (_ == BigInt(chunk * 10 * (chunk * 10 + 1L) / 2))
+  expected foreach { x => println("OK") }
 
-  val xsf: Future[IndexedSeq[BigInt]] = Future.sequence(xfs)
-
-  val xf: Future[BigInt] = for (ls <- xsf) yield sum(ls.toStream)
-
-  xf onComplete {
-    case Success(z) => println(s"Total is: $z")
-    case Failure(t) => System.err.println("An error has occured: " + t.getMessage)
-  }
-
-  Await.ready(xf, 10000 milli)
-
-  println("goodbye")
-
+  Await.ready(expected, 10000 milli)
+  println("Goodbye")
 }
