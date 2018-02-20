863d320057e5c9b827349fc639717654ea8a95ff
yingyuan <yingy4@hotmail.com>
2018-02-20 18:07:38 -0500

    Lab-AwsCloudFormation: Add TODO label

--- lab-awscloudformation/src/test/scala/edu/neu/coe/csye7200/lbawscld/S3BucketSpec.scala
+++ lab-awscloudformation/src/test/scala/edu/neu/coe/csye7200/lbawscld/S3BucketSpec.scala
@@ -1,11 +1,11 @@
 package edu.neu.coe.csye7200.lbawscld
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 import spray.json._
 
-class S3BucketSpec extends FunSpec with Matchers {
+class S3BucketSpec extends FlatSpec with Matchers {
 
-  it ("should generate s3 buckect json") {
+  it should "generate s3 buckect json" in {
 
     S3Bucket.simpleTemplate.toJson.toString() shouldBe "{\"AWSTemplateFormatVersion\":\"2010-09-09\",\"Description\":\"Simple S3 Bucket template\",\"Parameters\":{\"DomainName\":{\"Type\":\"String\"}},\"Resources\":{\"testS3Bucket\":{\"Properties\":{\"BucketName\":{\"Fn::Join\":[\".\",[\"csye7200\",{\"Ref\":\"DomainName\"},\"lab\"]]}},\"Type\":\"AWS::S3::Bucket\"}}}"
   }
