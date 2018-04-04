ee65a3dda6262bda1984c5bf270b527332034e0a
yingy4 <yingy4@hotmail.com>
2018-04-05 07:19:20 +0800

    Tag all spark tests as Slow

--- assignment-spark-wordcount/src/test/scala/edu/neu/coe/csye7200/asstswc/WordCountSpec.scala
+++ assignment-spark-wordcount/src/test/scala/edu/neu/coe/csye7200/asstswc/WordCountSpec.scala
@@ -1,6 +1,7 @@
 package edu.neu.coe.csye7200.asstswc
 
 import org.apache.spark.sql.SparkSession
+import org.scalatest.tagobjects.Slow
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class WordCountSpec extends FlatSpec with Matchers with BeforeAndAfter  {
@@ -23,7 +24,7 @@ class WordCountSpec extends FlatSpec with Matchers with BeforeAndAfter  {
 
   behavior of "Spark"
 
-  ignore should "work for wordCount" in {
+  ignore should "work for wordCount" taggedAs Slow in {
     WordCount.wordCount(spark.read.textFile(getClass.getResource("WordCount.txt").getPath).rdd," ").collect() should matchPattern {
       case Array(("Hello",3),("World",3),("Hi",1)) =>
     }
