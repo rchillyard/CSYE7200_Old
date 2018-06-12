4e8407acd1778344cda95772c8c345098315ca7c
Scala Prof <scalaprof@gmail.com>
2018-06-12 13:36:15 -0400

    Args: finished implementation
    (cherry picked from commit 84987b4)

--- functional-programming/src/main/scala/edu/neu/coe/csye7200/Args.scala
+++ functional-programming/src/main/scala/edu/neu/coe/csye7200/Args.scala
@@ -11,12 +11,12 @@ case class Arg[X](name: Option[String], value: Option[X]) {
   def map[Y](f: X => Y): Arg[Y] = Arg(name, value map f)
 
   def process(fm: Map[String, Option[X] => Unit]): Try[Option[X]] = {
-    def processFunc(fo: Option[Option[X] => Unit]): Try[Option[X]] = fo match {
-      case Some(f) => val xoy = Try(f(value)); xoy.map(_ => None)
+    def processFuncMaybe(fo: Option[Option[X] => Unit]): Try[Option[X]] = fo match {
+      case Some(f) => Try(f(value)).map(_ => None)
       case None => Failure(AnonymousNotFoundException)
     }
 
-    def process(c: String): Try[Option[X]] = processFunc(fm.get(c)).recoverWith({ case AnonymousNotFoundException => Failure(NotFoundException(c)) })
+    def process(c: String): Try[Option[X]] = processFuncMaybe(fm.get(c)).recoverWith({ case AnonymousNotFoundException => Failure(NotFoundException(c)) })
 
     name match {
       case Some(c) => process(c)
@@ -31,7 +31,7 @@ object Arg {
   def apply(c: String, a: String): Arg[String] = Arg(Some(c), Some(a))
 }
 
-case class Args[X](xas: Seq[Arg[X]]) {
+case class Args[X](xas: Seq[Arg[X]]) extends Traversable[Arg[X]] {
   def map[Y](f: X => Y): Args[Y] = Args(for (xa <- xas) yield xa.map(f))
 
   def process(fm: Map[String, Option[X] => Unit]): Try[Seq[X]] =
@@ -39,61 +39,26 @@ case class Args[X](xas: Seq[Arg[X]]) {
       case Success(xos) => Success(xos.flatten)
       case Failure(x) => Failure(x)
     }
-}
 
-class SimpleArgParser extends JavaTokenParsers {
-  def parseToken(s: String): Try[Token] = parseAll(token, s) match {
-    case Success(t, _) => scala.util.Success(t)
-    case _ => scala.util.Failure(new Exception(s"could not parse '$s' as a token"))
-  }
+  def iterator: Iterator[Arg[X]] = xas.iterator
 
-  trait Token {
-    def s: String
-  }
+  def foreach[U](f: Arg[X] => U): Unit = xas foreach f
 
-  case class Command(s: String) extends Token
-
-  case class Argument(s: String) extends Token
-
-  case object NotAToken extends Token {
-    def s: String = null
-  }
-
-  def token: Parser[Token] = command | argument
-
-  def command: Parser[Command] = "-" ~> cmdR ^^ (s => Command(s))
-
-  def argument: Parser[Argument] = argR ^^ (s => Argument(s))
-
-  def toCmd(w: String): Arg[String] = Arg(Some(w), None)
-
-  private val cmdR = """[a-z]+""".r
-  private val argR = """\w+""".r
-}
-
-class PosixArgParser extends JavaTokenParsers {
-
-  def posixCommandLine: Parser[Args[String]] = rep(posixArgs) ^^ (as => Args(as.flatten))
-
-  def posixArgs: Parser[List[Arg[String]]] = cmdR ~ rep(cmdR) ~ opt(argR) ^^ { case c ~ cs ~ a => (c :: cs.init).map(toCmd) :+ Arg(Some(cs.last), a) }
-
-  def toCmd(w: String): Arg[String] = Arg(Some(w), None)
-
-  private val cmdR = """-?([a-z])""".r
-  val argR = """\s(\w+)""".r
 }
 
 object Args {
-  val p = new SimpleArgParser
+  def create(args: Arg[String]*): Args[String] = apply(args)
 
-  def inner(r: Seq[Arg[String]], w: Seq[p.Token]): Seq[Arg[String]] = w match {
-    case Nil => r
-    case p.Command(c) :: p.Argument(a) :: t => inner(r :+ Arg(c, a), t)
-    case p.Command(c) :: t => inner(r :+ Arg(c), t)
-    case p.Argument(a) :: t => inner(r :+ Arg(None, Some(a)), t)
-  }
+  def parse(args: Array[String]): Args[String] = {
+    val p = new SimpleArgParser
 
-  def apply(args: Array[String]): Args[String] = {
+    def inner(r: Seq[Arg[String]], w: Seq[p.Token]): Seq[Arg[String]] = w match {
+      case Nil => r
+      case p.Command(c) :: p.Argument(a) :: t => inner(r :+ Arg(c, a), t)
+      case p.Command(c) :: t => inner(r :+ Arg(c), t)
+      case p.Argument(a) :: t => inner(r :+ Arg(None, Some(a)), t)
+    }
+
     val tys = for (a <- args) yield p.parseToken(a)
     val ts = MonadOps.sequence(tys) match {
       case Success(ts_) => ts_
@@ -117,7 +82,42 @@ object Args {
   //      case p.Error(e, _) => Failure(ParseException(e))
   //      case _ => Failure(new Exception("logic error"))
   //    }
+}
 
+class SimpleArgParser extends JavaTokenParsers {
+  def parseToken(s: String): Try[Token] = parseAll(token, s) match {
+    case Success(t, _) => scala.util.Success(t)
+    case _ => scala.util.Failure(new Exception(s"could not parse '$s' as a token"))
+  }
+
+  trait Token {
+    def s: String
+  }
+
+  case class Command(s: String) extends Token
+
+  case class Argument(s: String) extends Token
+
+  def token: Parser[Token] = command | argument
+
+  def command: Parser[Command] = "-" ~> cmdR ^^ (s => Command(s))
+
+  def argument: Parser[Argument] = argR ^^ (s => Argument(s))
+
+  private val cmdR = """[a-z]+""".r
+  private val argR = """\w+""".r
+}
+
+class PosixArgParser extends JavaTokenParsers {
+
+  def posixCommandLine: Parser[Args[String]] = rep(posixArgs) ^^ (as => Args(as.flatten))
+
+  def posixArgs: Parser[List[Arg[String]]] = cmdR ~ rep(cmdR) ~ opt(argR) ^^ { case c ~ cs ~ a => (c :: cs.init).map(toCmd) :+ Arg(Some(cs.last), a) }
+
+  def toCmd(w: String): Arg[String] = Arg(Some(w), None)
+
+  private val cmdR = """-?([a-z])""".r
+  private val argR = """\s(\w+)""".r
 }
 
 case object AnonymousNotFoundException extends Exception()
