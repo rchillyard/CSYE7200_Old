5b219561707453b40535b14533a4507edf3c60a9
Scala Prof <scalaprof@gmail.com>
2018-06-15 12:13:33 -0400

    Args: improvements
    (cherry picked from commit 78615c4)

--- functional-programming/src/main/scala/edu/neu/coe/csye7200/Args.scala
+++ functional-programming/src/main/scala/edu/neu/coe/csye7200/Args.scala
@@ -8,7 +8,26 @@ import scala.util._
 import scala.util.parsing.combinator.JavaTokenParsers
 
 case class Arg[X](name: Option[String], value: Option[X]) {
+
+  def byName(w: String): Option[Arg[X]] = name match {
+    case Some(`w`) => Some(this)
+    case _ => None
+  }
+
   def map[Y](f: X => Y): Arg[Y] = ??? // TODO implement me
+
+  def asMaybeTuple: Option[(String, X)] = name match {
+    case Some(w) => value match {
+      case Some(x) => Some((w, x))
+      case _ => None
+    }
+    case _ => None
+  }
+
+  def toY[Y: Derivable]: Y = value match {
+    case Some(x) => implicitly[Derivable[Y]].deriveFrom(x)
+    case _ => throw NoValueException(name)
+  }
 
   def process(fm: Map[String, Option[X] => Unit]): Try[Option[X]] = {
     def processFuncMaybe(fo: Option[Option[X] => Unit]): Try[Option[X]] = fo match {
@@ -23,6 +42,8 @@ case class Arg[X](name: Option[String], value: Option[X]) {
       case None => Success(value)
     }
   }
+
+  override def toString: String = s"Arg: command ${name.getOrElse("anonymous")} with value: ${value.getOrElse("none")}"
 }
 
 object Arg {
@@ -33,6 +54,21 @@ object Arg {
 
 case class Args[X](xas: Seq[Arg[X]]) extends Traversable[Arg[X]] {
   def map[Y](f: X => Y): Args[Y] = ??? // TODO implement me
+
+  def extract: Map[String, X] = (for (xa <- xas) yield xa.asMaybeTuple).flatten.toMap
+
+  /**
+    * Method to get an Arg whose name matches the given string.
+    *
+    * @param w the string to match
+    * @return Some(arg) if the name matches, else None
+    */
+  def getArg(w: String): Option[Arg[X]] = {
+    MonadOps.sequence(for (xa <- xas) yield xa.byName(w)) match {
+      case Some(xas_) => if (xas_.size == 1) Some(xas_.head) else throw AmbiguousNameException(w)
+      case _ => None
+    }
+  }
 
   def process(fm: Map[String, Option[X] => Unit]): Try[Seq[X]] =
     MonadOps.sequence(for (xa <- xas) yield for (x <- xa.process(fm)) yield x) match {
@@ -84,6 +120,22 @@ object Args {
   //    }
 }
 
+/**
+  * Type-class trait to allow conversion from type X to type T
+  *
+  * @tparam T the result type
+  */
+trait Derivable[T] {
+  /**
+    * Method to convert an X to a T
+    *
+    * @param x the X value
+    * @tparam X the input type
+    * @return a T
+    */
+  def deriveFrom[X](x: X): T
+}
+
 class SimpleArgParser extends JavaTokenParsers {
   def parseToken(s: String): Try[Token] = parseAll(token, s) match {
     case Success(t, _) => scala.util.Success(t)
@@ -120,8 +172,14 @@ class PosixArgParser extends JavaTokenParsers {
   private val argR = """\s(\w+)""".r
 }
 
-case object AnonymousNotFoundException extends Exception()
+abstract class ArgsException(s: String) extends Exception(s"Args exception: $s")
 
-case class NotFoundException(command: String) extends Exception(s"Arg: command $command not found")
+case object AnonymousNotFoundException extends ArgsException("no anonymous arg found")
 
-case class ParseException(cause: String) extends Exception(cause)
+case class NotFoundException(command: String) extends ArgsException(s"Arg: command $command not found")
+
+case class AmbiguousNameException(name: String) extends ArgsException(s"$name ambiguous")
+
+case class ParseException(cause: String) extends ArgsException(cause)
+
+case class NoValueException(name: Option[String]) extends ArgsException(s"Arg: command ${name.getOrElse("anonymous")} has no value")
