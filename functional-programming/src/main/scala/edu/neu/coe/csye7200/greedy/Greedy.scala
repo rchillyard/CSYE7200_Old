5fb1bb8ba111d598061a9589f19896c8a040fee9
Robin Hillyard <scalaprof@gmail.com>
2018-08-30 11:02:29 -0400

    Added Greedy package to FunctionalProgramming
    Specifically, added the Zeckendorf implementation based on Greedy. More or less this is to demonstrate how Scala differs from Java.
    
    (cherry picked from commit d3e25ba)

---
+++ functional-programming/src/main/scala/edu/neu/coe/csye7200/greedy/Greedy.scala
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2018. Phasmid Software
+ */
+
+package edu.neu.coe.csye7200.greedy
+
+import java.util.function.{BiFunction, Function}
+
+import scala.annotation.tailrec
+
+case class Greedy[T, R](fGreedy: T=>T, fAdjust: (T,T)=>T, fResult: (T, R) => R, fDone: T=>Boolean) extends ((T, R) => R) {
+  override def apply(t: T, r: R): R = {
+
+  @tailrec def inner(_t: T, _r: R): R =
+    if (fDone(_t)) _r
+    else {
+      val greedy = fGreedy.apply(_t)
+      inner(fAdjust.apply(_t, greedy), fResult.apply(greedy, _r))
+    }
+
+  inner(t, r)
+}
+}
