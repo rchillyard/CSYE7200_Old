ee29cef262df4916c0ef57af0b00bcedb109b3a1
Robin Hillyard <scalaprof@gmail.com>
2018-09-16 11:11:09 -0400

    Added laziness package; Minor refactorings.
    (cherry picked from commit 8679e60)

---
+++ functional-programming/src/test/scala/edu/neu/coe/csye7200/laziness/MyStreamTest.scala
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018. Phasmid Software
+ */
+
+package edu.neu.coe.csye7200.laziness
+
+import org.scalatest.FunSuite
+
+
+
+//class MyStreamTest extends FunSuite {
+//
+//  test("MyStream should produce a stream of 1s") {
+//    val x: MyStream[Int] = Cons(1,  () => x)
+//
+//    val y = x take 3
+//    assert(y.size==3)
+//  }
+//}
