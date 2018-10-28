bcfe5f5762b3e6f153fecf22ff7c82a288135e07
chugh.k <chugh.k@husky.neu.edu>
2018-10-28 15:05:34 -0400

    updating asst-cache with TODOs implemented

--- asst-cache/src/main/scala/edu/neu/coe/csye7200/cache/Cache.scala
+++ asst-cache/src/main/scala/edu/neu/coe/csye7200/cache/Cache.scala
@@ -6,6 +6,7 @@ package edu.neu.coe.csye7200.cache
 
 import scala.collection.mutable
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait Cache[K, V] extends (K=>Future[V]) {
 
@@ -20,7 +21,7 @@ case class MyCache[K, V](fulfill: K=>Future[V]) extends Cache[K, V] {
 
   private def put(k: K, v: V): Unit = cache.+=((k,v))
 
-  override def apply(k: K): Future[V] = ??? // TODO
+  override def apply(k: K): Future[V] = if (cache.contains(k)) Future(cache(k)) else for (v <- fulfill(k); _ = put(k, v)) yield v
 
   def expire(k: K): Unit = cache.-=(k)
 
